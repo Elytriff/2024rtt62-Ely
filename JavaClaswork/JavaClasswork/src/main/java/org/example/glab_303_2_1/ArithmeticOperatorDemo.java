17d16c4c24c8f90706d0f251b6db341058b475d9
Ely <elytriff@gmail.com>
2024-05-02 21:33:31 -0400

    Glabs 303_2_1  and 303_2_2 , homework May2

---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/glab_303_2_1/ArithmeticOperatorDemo.java
@@ -0,0 +1,50 @@
+package org.example.glab_303_2_1;
+
+public class ArithmeticOperatorDemo {
+    public static void main(String[] args) {
+
+        int x, y = 10, z = 5;
+        x = y + z;
+        System.out.println("+ operator resulted in " + x);
+        x = y - z;
+        System.out.println("- operator resulted in " + x);
+        x = y * z;
+        System.out.println("* operator resulted in " + x);
+        x = y / z;
+        System.out.println("/ operator resulted in " + x);
+        x = y % z;// gives me the remainder
+        System.out.println("% operator resulted in " + x);
+
+        x = y++;//Assigns the current value of y to x,
+        // and then increments y by 1.
+        // So, x gets the value 10, and y becomes 11.
+        System.out.println("Postfix ++ operator resulted in " + x);
+        //System.out.println("y ++ operator resulted in " + y++);
+
+        x = ++z;// Increments z by 1 and then assigns the incremented value to x.
+        // So, z becomes 6, and x gets the value 6.
+        System.out.println("Prefix ++ operator resulted in " + x);
+        //System.out.println("++z operator resulted in " + ++z);
+
+        x = -y;
+        System.out.println("Unary operator resulted in " + x);
+
+        // Some examples of special Cases
+        int tooBig = Integer.MAX_VALUE + 1; // -2147483648 which is
+        // Integer.MIN_VALUE.
+        int tooSmall = Integer.MIN_VALUE - 1; // 2147483647 which is
+        // Integer.MAX_VALUE.
+        System.out.println("tooBig becomes " + tooBig);
+        System.out.println("tooSmall becomes " + tooSmall);
+        System.out.println(4.0 / 0.0); // Prints: Infinity
+        System.out.println(-4.0 / 0.0); // Prints: -Infinity
+        System.out.println(0.0 / 0.0); // Prints: NaN
+        double d1 = 12 / 8; // result: 1 by integer division. The value of d1 becomes 1.0.
+        double d2 = 12.0F / 8; // result: 1.5
+        System.out.println("d1 is " + d1);
+        System.out.println("d2 is " + d2);
+
+        //String message = 100 + "Messages"; //"100 Messages"??
+
+    }
+}
---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/glab_303_2_1/AssignmentOperatorsDemo.java
@@ -0,0 +1,19 @@
+package org.example.glab_303_2_1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args){
+        int j = 10;// gets the value 10
+        j = 5; // value is overwritten to 5 (j has been recycle)
+        int k = j;// k gets 5's value
+        System.out.println("j is: " + j);
+        System.out.println("k is: " + k);
+        //              Multiple Assignments
+        k = j = 10; // (k = (j = 10))
+        System.out.println("j is: " + j);
+        System.out.println("k is: " + k);
+
+    }
+
+}
