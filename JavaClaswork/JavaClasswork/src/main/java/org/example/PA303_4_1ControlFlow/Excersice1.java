ea21f964e1901d67db56eaf34b5e8249d6eb7a08
Ely <elytriff@gmail.com>
2024-05-05 03:44:52 -0400

    PA 303.4.1 control flow home work, due May 6

---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/PA303_4_1ControlFlow/Excersice1.java
@@ -0,0 +1,17 @@
+package org.example.PA303_4_1ControlFlow;
+
+public class Excersice1 {
+    public static void main(String[] args){
+        //1. Write a program that declares 1 integer variable x, and then assigns 7 to it.
+        // Write an if statement to print out “Less than 10” if x is less than 10.
+        // Change x to equal 15, and notice the result (console should not display anything).
+        int x = 7;
+        if (x<10){
+            System.out.println("Les than ten");
+        }
+        int x2 = 15;
+        if (x2<10){
+            System.out.println("Les than ten");
+        }
+    }
+}
