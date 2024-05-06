477a2468309748eebe146028373b5dacd2352de4
Ely <elytriff@gmail.com>
2024-05-05 23:11:51 -0400

    no message

---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/glab_303_5_1_Forloop/PredictFutureTuittion.java
@@ -0,0 +1,24 @@
+package org.example.glab_303_5_1_Forloop;
+
+import jdk.jfr.Percentage;
+
+public interface PredictFutureTuittion {
+    public static void main(String[] Args) {
+        //Predict Future Tuition
+        //Suppose the tuition for a university is $10,000 for the current year and increases by 7 percent every year
+        // In how many years will the tuition be doubled?
+
+        int currentTuition = 10000;
+        float percentage = currentTuition * (7f / 100);
+        int counter = 0;
+        //System.out.println(percentage);
+        System.out.println("This year tuition is: " + currentTuition);
+        float x = currentTuition + percentage;// initializing first count to start at 10700
+        while (x <= currentTuition * 2) {
+            System.out.println("Next year tuition is: " + (int)x);
+            x = x + percentage;
+            counter ++;
+        }
+        System.out.println("Tuition will double in: " + (counter) + " years.");
+    }
+}
