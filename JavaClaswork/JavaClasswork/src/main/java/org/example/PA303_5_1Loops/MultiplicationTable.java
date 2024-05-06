82bc901159849e4401a92fa7737d636f57a31f34
Ely <elytriff@gmail.com>
2024-05-05 20:55:46 -0400

    Home work due May6

---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/PA303_5_1Loops/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package org.example.PA303_5_1Loops;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        //Print a Multiplication Table
+        //Write a program that uses nested for loops to print a multiplication table.
+        int x;
+        int f;
+
+        for (x = 1; x <= 12; ++x) {      // outer loop
+            for (f = 1; f <= 12; ++f){     // inner loop
+                System.out.print(" " + (x * f));
+           }
+            System.out.println();
+        }
+    }
+}
---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/glab_303_5_1_Forloop/Loopdemo.java
@@ -0,0 +1,12 @@
+package org.example.glab_303_5_1_Forloop;
+
+public class Loopdemo {
+    // Program to print a text 5 times
+        public static void main(String[] args) {
+            int n = 5;
+            // for loop
+            for (int i = 1; i <= n; ++i) {
+                System.out.println("Java is fun");
+            }
+        }
+    }
