97eb008cc725f1ab4496bc6b30304faabd04741a
Ely <elytriff@gmail.com>
2024-05-09 22:40:41 -0400

    organizing files

--- JavaClaswork/JavaClasswork/src/main/java/org/example/HomeworkMay7and9/Glab303_9_1Encapsulation/DemoOne.java
+++
@@ -1,8 +0,0 @@
-package org.example.HomeworkMay7and9.Glab303_9_1Encapsulation;
-
-public class DemoOne {
-    static int var1 = 20;
-    static String var2 = "perscholas";
-    int var3 = 100;
-
-}
--- JavaClaswork/JavaClasswork/src/main/java/org/example/HomeworkMay7and9/Glab303_9_2InheritanceDemo/Hospital.java
+++
@@ -1,9 +0,0 @@
-package org.example.HomeworkMay7and9.Glab303_9_2InheritanceDemo;
-
-public class Hospital {
-    public static void main(String args[]) {
-        Surgeon s = new Surgeon();
-        s.Doctor_Details();
-        s.Surgeon_Details();
-    }
-}
