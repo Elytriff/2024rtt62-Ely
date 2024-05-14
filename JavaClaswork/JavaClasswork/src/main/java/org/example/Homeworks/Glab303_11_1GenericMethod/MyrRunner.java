616b11dfe8dad09ce34f441ec6b57c468376bd3b
Ely <elytriff@gmail.com>
2024-05-14 09:29:23 -0400

    Glabs 303_11_1 and 2

---
+++ JavaClaswork/JavaClasswork/src/main/java/org/example/Homeworks/Glab303_11_1GenericMethod/MyrRunner.java
@@ -0,0 +1,11 @@
+package org.example.Homeworks.Glab303_11_1;
+
+public class MyrRunner {public static void main(String[] args) {
+// initialize the class with Integer data
+    DemoClass dObj = new DemoClass();
+    dObj.genericsMethod(25); // passing int
+    dObj.genericsMethod("Per Scholas"); // passing String
+    dObj.genericsMethod(2563.5); // passing float
+    dObj.genericsMethod('H'); // passing Char
+ }
+}
